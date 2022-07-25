f6cb95d421b70768d7efe271fe7e33515f3db048
José Matheus S. Rodrigues <107767538+0drigues@users.noreply.github.com>
2022-07-25 16:52:49 -0300

    Add files via upload
    Replacement

--- 7-funcoes/7-3_func-arrow.js
+++ 7-funcoes/7-3_func-arrow.js
@@ -1,32 +1,32 @@
-function calculadora(num1, num2){
-    var num1, num2;
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
+document.write("<h1> CALCULADORA COM ARROW FUNCTIONS </h1>");
+document.write("<br><hr>");
 
-    if (op === "+"){
-        soma = (num1, num2) => {return num1 + num2};
-    }
+var num1, num2, op;
 
-    else if (op === "-"){
-        subtrac = (num1, num2) => {return num1 - num2};
-    }
-
-    else if (op === "*"){
-        multipl = (num1, num2) => {return num1 * num2};
-    }
-
-    else if (op === "/"){
-        div = (num1, num2) => {return num1 / num2};
-    }
-}
-
-var num1, num2, op, resultado_final;
 num1 = window.prompt("Digite o primeiro valor: ");
 num1 = parseInt(num1);
 num2 = window.prompt("Digite o segundo valor: ");
 num2 = parseInt(num2);
 op = window.prompt("Digite a operação desejada: +, -, * ou /");
 
-resultado_final = calculadora(num1, num2);
+if (op === "+"){
+    var soma = (num1, num2) => {return resultado = num1 + num2};
+    document.write("O resultado da soma é: ", num1 + num2);
+}
 
-document.write("Resultado da operação: ", + resultado_final);
+else if (op === "-"){
+    var subtrac = (num1, num2) => {return num1 - num2};
+    document.write("O resultado da subtração é: ", num1 - num2);
+}
+
+else if (op === "*"){
+    var multipl = (num1, num2) => {return num1 * num2};
+    document.write("O resultado da multiplicação é: ", num1 * num2);
+}
+
+else {
+    var div = (num1, num2) => {return num1 / num2};
+    document.write("O resultado da divisão é: ", num1 / num2);
+}
+
+document.write("<br><br>Fim :)");
